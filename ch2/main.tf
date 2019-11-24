0733a5cc4609403d6574d9f0587f68b5ffe9f2f1
Andy D <mindovermiles262@gmail>
2019-11-24 15:30:40 -0800

    use tf variables

--- ch2/main.tf
+++ ch2/main.tf
@@ -3,12 +3,20 @@ provider "aws" {
 }
 
 
-resource "aws_security_group" "webserver_dmz_8080" {
-  name = "terraform-webserver-8080"
+variable "server_port" {
+  description = "The port the webserver will use for HTTP requests"
+  type        = number
+  default     = 8080
+}
+
+
+resource "aws_security_group" "tf_webserver_dmz" {
+  description = "TF Webserver DMZ. Allow ${var.server_port}/TCP"
+  name        = "terraform-webserver-${var.server_port}"
 
   ingress {
-    from_port   = 8080
-    to_port     = 8080
+    from_port   = var.server_port
+    to_port     = var.server_port
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -16,18 +24,23 @@ resource "aws_security_group" "webserver_dmz_8080" {
 
 
 resource "aws_instance" "webserver" {
-  ami                     = "ami-06d51e91cea0dac8d"   #=> Ubuntu 18.04LTS
-  instance_type           = "t2.micro"
-  vpc_security_group_ids  = [aws_security_group.webserver_dmz_8080.id]
+  ami                    = "ami-06d51e91cea0dac8d" #=> Ubuntu 18.04LTS
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.tf_webserver_dmz.id]
 
   user_data = <<-EOF
-#!/bin/bash
-echo "Terraform Life" > index.html
-nohup busybox httpd -f -p 8080 &
-EOF
-
+              #!/bin/bash
+              echo "Terraform Life" > index.html
+              nohup busybox httpd -f -p ${var.server_port} &
+              EOF
   tags = {
     Name = "Terraform Example"
   }
+}
+
+
+output "server_public_ip" {
+  description = "The Public IP of the TF Web Server"
+  value       = aws_instance.webserver.public_ip
 }
 
